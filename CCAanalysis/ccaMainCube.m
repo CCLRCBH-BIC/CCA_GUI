2e688b67973ec4da8d1d8cb1d08c0de2598cb332
ZYANG <yzhengshi@gmail.com>
2023-01-30 21:05:58 -0800

    Update ccaMainCube.m

--- CCAanalysis/ccaMainCube.m
+++ CCAanalysis/ccaMainCube.m
@@ -26,31 +26,46 @@ elseif numel(siz)==3
     siz = [siz,1];
     Y = reshape(Y,siz); mask = reshape(mask,siz(2:end));
 end
-Cor = zeros(size(mask));
-Statistic = cell(size(mask));
-Alp = cell(size(mask));
-Beta = cell(size(mask));
-opt_Yind = cell(size(mask));
+
 
 fMRIdata = parallel.pool.Constant(Y);
 Design = parallel.pool.Constant(X);
+% fMRIdata = [];fMRIdata.Value = Y;
+% Design = [];Design.Value = X;
 voxel_ind = find(mask(:)>0);
 voxel_ind = reshape(voxel_ind,1,numel(voxel_ind));
+nvoxel = numel(voxel_ind);
 tic
-parfor i = voxel_ind
-    [ix,iy,iz] = ind2sub(siz(2:end),i);
+Cor_q = zeros(nvoxel,1);
+Statistic_q = cell(nvoxel,1);
+Alp_q = cell(nvoxel,1);
+Beta_q = cell(nvoxel,1);
+opt_Yind_q = cell(nvoxel,1);
+parfor i = 1:nvoxel
+    [ix,iy,iz] = ind2sub(siz(2:end),voxel_ind(i));
     temp_mask = zeros(siz(2:end));
     temp_mask(max([ix-nneigh,1]):min([ix+nneigh,siz(2)]),max([iy-nneigh,1]):min([iy+nneigh,siz(3)]),...
         max([iz-nneigh,1]):min([iz+nneigh,siz(4)])) = 1;
     temp_mask(ix,iy,iz) = 0;
-    Y = [fMRIdata.Value(:,ix,iy,iz),fMRIdata.Value(:,temp_mask>0)];
-    [Sxx,Sxy,Syx,Syy] = corrCombo(Design.Value,Y,flag);
+    temp_Y = [fMRIdata.Value(:,ix,iy,iz),fMRIdata.Value(:,temp_mask>0)];
+    [Sxx,Sxy,Syx,Syy] = corrCombo(Design.Value,temp_Y,flag);
     if Syy(1,1)~=0
         S = struct('Sxx',Sxx,'Sxy',Sxy,'Syx',Syx,'Syy',Syy);
-        [cor,alp,beta,statistic,opt_yind]=ccaCombo(X,Y,C,S,flag,mode);
-        Cor(i)=cor;Alp{i}=alp;Beta{i}=beta;Statistic{i}=statistic;opt_Yind{i}=opt_yind;
+        [cor,alp,beta,statistic,opt_yind]=ccaCombo(X,temp_Y,C,S,flag,mode);
+        Cor_q(i)=cor;Alp_q{i}=alp;Beta_q{i}=beta;Statistic_q{i}=statistic;opt_Yind_q{i}=opt_yind;
     end
 end
 toc
+Cor = zeros(nvoxel,1);
+Statistic = cell(nvoxel,1);
+Alp = cell(nvoxel,1);
+Beta = cell(nvoxel,1);
+opt_Yind = cell(nvoxel,1);
+Cor(mask>0) = Cor_q;
+Statistic(mask>0) = Statistic_q;
+Alp(mask>0) = Alp_q;
+Beta(mask>0) = Beta_q;
+opt_Yind(mask>0) = opt_Yind_q;
+
 end
 
