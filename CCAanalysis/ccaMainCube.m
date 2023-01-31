3661bc94c6982445693db0da3de6d75e5716e630
ZYANG <yzhengshi@gmail.com>
2023-01-30 20:35:25 -0800

    Update ccaMainCube.m
    The sharedmemory approach used previous is not compatible with newer Matlab version. New way to share memory across workers is used.

--- CCAanalysis/ccaMainCube.m
+++ CCAanalysis/ccaMainCube.m
@@ -1,4 +1,4 @@
-function [Cor,Alp,Beta,Statistic,opt_Yind]=ccaMainCube(X,Y,mask,C,flag,mode)
+function [Cor,Alp,Beta,Statistic,opt_Yind]=ccaMainCube(X,Y,mask,C,flag,mode,nneigh)
 %% [Cor,Alp,Beta,Statistic,opt_Yind]=ccaMain(X,Y,mask,C,flag,mode)
 % example:
 %   Cor=ccaMain(X,reshape(simu_data,278,32,32),[],[0;-1;1;0],'Pearson','sum');
@@ -17,49 +17,40 @@ end
 if isempty(C)
     C=zeros(size(X,2),1);C(1)=1;
 end
-
+if exist('nneigh','var')==0 || isempty(nneigh)
+    nneigh=1;
+end
 if numel(siz)<3
     error('ccaMain: at least one slice should be considered');
 elseif numel(siz)==3
     siz = [siz,1];
     Y = reshape(Y,siz); mask = reshape(mask,siz(2:end));
 end
-N_X = size(X,2);
-N_voxperslice = siz(2)*siz(3);
+Cor = zeros(size(mask));
+Statistic = cell(size(mask));
+Alp = cell(size(mask));
+Beta = cell(size(mask));
+opt_Yind = cell(size(mask));
 
-NoofSlice = siz(end);
-Cor = zeros(siz(2)*siz(3),NoofSlice);
-Statistic = cell(siz(2)*siz(3),NoofSlice);
-Alp = cell(siz(2)*siz(3),NoofSlice);
-Beta = cell(siz(2)*siz(3),NoofSlice);
-opt_Yind = cell(siz(2)*siz(3),NoofSlice);
-
-n=1;
-for i = 2:NoofSlice-1
-    temp_Y = reshape(Y(:,:,:,i-1:i+1),siz(1),[]);
-    temp_mask = squeeze(mask(:,:,i-1:i+1));temp_mask = temp_mask(:);
-    shmkey_Y = [flag,'GMshmkey_Y'];   
-    shmobj_Y = shmobject(shmkey_Y,temp_Y);  
-    disp(['slice ',num2str(i),': CCA is running.']);
-    parfor q = 1:N_voxperslice %% can be parfor
-%         disp(q);
-        robj_Y = shmref(shmkey_Y);  
-        [x_ind,y_ind] = ind2sub(siz(2:3),q);
-        if temp_mask(q) == 0 || min(y_ind,x_ind)<=n || x_ind>siz(2)-n || y_ind>siz(3)-n
-        else
-            [ix,iy,iz]=meshgrid(x_ind-n:x_ind+n,y_ind-n:y_ind+n,1:3);
-            neigh = sub2ind([siz(2:3),3],ix,iy,iz);neigh=neigh([14,1:13,15:27]);
-            neigh = neigh(temp_mask(neigh)==1);  
-            [Sxx,Sxy,Syx,Syy] = corrCombo(X,robj_Y.data(:,neigh),flag);
-            if Syy(1,1)~=0
-                S = struct('Sxx',Sxx,'Sxy',Sxy,'Syx',Syx,'Syy',Syy);
-                [cor,alp,beta,statistic,opt_yind]=ccaCombo(X,robj_Y.data(:,neigh),C,S,flag,mode);
-                Cor(q,i)=cor;Alp{q,i}=alp;Beta{q,i}=beta;Statistic{q,i}=statistic;opt_Yind{q,i}=opt_yind;
-            end
-        end
-        delete(robj_Y);
+fMRIdata = parallel.pool.Constant(Y);
+Design = parallel.pool.Constant(X);
+voxel_ind = find(mask(:)>0);
+voxel_ind = reshape(voxel_ind,1,numel(voxel_ind));
+tic
+parfor i = voxel_ind
+    [ix,iy,iz] = ind2sub(siz(2:end),i);
+    temp_mask = zeros(siz(2:end));
+    temp_mask(max([ix-nneigh,1]):min([ix+nneigh,siz(2)]),max([iy-nneigh,1]):min([iy+nneigh,siz(3)]),...
+        max([iz-nneigh,1]):min([iz+nneigh,siz(4)])) = 1;
+    temp_mask(ix,iy,iz) = 0;
+    Y = [fMRIdata.Value(:,ix,iy,iz),fMRIdata.Value(:,temp_mask>0)];
+    [Sxx,Sxy,Syx,Syy] = corrCombo(Design.Value,Y,flag);
+    if Syy(1,1)~=0
+        S = struct('Sxx',Sxx,'Sxy',Sxy,'Syx',Syx,'Syy',Syy);
+        [cor,alp,beta,statistic,opt_yind]=ccaCombo(X,Y,C,S,flag,mode);
+        Cor(i)=cor;Alp{i}=alp;Beta{i}=beta;Statistic{i}=statistic;opt_Yind{i}=opt_yind;
     end
 end
-
+toc
 end
-    
+
